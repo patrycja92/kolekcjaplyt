d11316983293ccb34dbb1cfc2d6a140aa69e0cea
patrycja92 <pysiak16@gmail.com>
2015-01-22 09:10:51 +0100

    Kolekcja

---
+++ src/kolekcjaplyt/Kolekcja.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kolekcjaplyt;
+
+/**
+ *
+ * @author Patrycja
+ */
+public class Kolekcja {
+    
+    private String nazwaplyty;
+    private int iloscplyt;
+    private String gatunek;
+    private String wydawnictwo;
+    
+
+    public String getNazwaplyty() {
+        return nazwaplyty;
+    }
+
+    public void setNazwaplyty(String nazwaplyty) {
+        this.nazwaplyty = nazwaplyty;
+    }
+
+    public String getWydawnictwo() {
+        return wydawnictwo;
+    }
+
+    public void setWydawnictwo(String wydawnictwo) {
+        this.wydawnictwo = wydawnictwo;
+    }
+   
+    public int getIloscplyt() {
+        return iloscplyt;
+    }
+
+    public void setIloscplyt(int iloscplyt) {
+        this.iloscplyt = iloscplyt;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public void setGatunek(String gatunek) {
+        this.gatunek = gatunek;
+    }
+    
+    
+}
